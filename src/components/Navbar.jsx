35829611f6be5b55f20b2c3557ffbb47193b00ea
Biradar Abhay Kapil <1032211780@mitwpu.edu.in>
2025-03-05 04:09:18 +0000

    Changes in the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ export const Navbar = ({menuOpen, setMenuOpen}) => {
             <div className="flex justify-between items-center h-16">
                 <a href="#home" className="font-mono text-xl font-bold text-white">
                     {" "}
-                    A.K <span className="text-blue-500">.Techy</span>
+                    Tech<span className="text-blue-500">.Bytes</span>
                     {" "}
 
                 </a>
